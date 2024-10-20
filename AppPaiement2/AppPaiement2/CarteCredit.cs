a7ec60d700243c6529a414452dab2ba77605416d
keundo Hugo Loic <300134106@monboreal.ca>
2024-10-19 21:14:31 -0400

    Ajout de la classe CarteCredit

---
+++ AppPaiement2/AppPaiement2/CarteCredit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace AppPaiement2
+{
+    internal class CarteCredit : Paiement
+    {
+        public int NumeroCarte {  get; set; }
+
+        public CarteCredit(double montant, string description, int numeroCarte) : base (montant ,description)
+        {
+            NumeroCarte = numeroCarte;
+        }
+        public override void AfficherDetails()
+        {
+            Console.WriteLine($": Montant {Montant}, Description: {Description}, NumeroCarte: {NumeroCarte}");
+        }
+    }
+}
--- AppPaiement2/AppPaiement2/Program.cs
+++ AppPaiement2/AppPaiement2/Program.cs
@@ -1,7 +1,21 @@
-﻿internal class Program
+﻿using AppPaiement2;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+        CarteCredit Paiement1 = new CarteCredit(100.20, "achat de chaussure", 854453258);
+        CarteCredit Paiement2 = new CarteCredit(200.50, "achat de livres", 985412032);
+        CarteCredit Paiement3 = new CarteCredit(80.60, "achat de nourriture", 547896521);
+
+        Paiement1.AfficherDetails();
+        Paiement2.AfficherDetails();
+        Paiement3.AfficherDetails();
+
+
+
+
     }
 }
